52f010d625cd4f77ed537cafa11899c44e3b6c2c
edmundsamina <edmundsamina@gmail.com>
2023-02-22 14:53:46 +0000

    connection established

--- home-grown/pages/messages.js
+++ home-grown/pages/messages.js
@@ -1,9 +1,11 @@
 import socketIO from "socket.io-client";
 
 export default function Messages() {
-  function handleClick() {
-    const socket = socketIO.connect("http://localhost:3001");
-    socket.on("messageResponse", console.log("hello"));
+function  handleClick() {
+    const socket = socketIO.connect("http://localhost:3001", {
+      withCredentials: true
+     })
+    socket.on("messageResponse", ()=>{console.log("hello")});
   }
 
   return <button onClick={handleClick}>Connect</button>;
