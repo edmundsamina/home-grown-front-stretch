8e416bfe26db943fa3f66b18176e63d7507c0eec
edmundsamina <edmundsamina@gmail.com>
2023-02-22 15:15:21 +0000

    basic fetch request

--- home-grown/pages/messages.js
+++ home-grown/pages/messages.js
@@ -1,12 +1,26 @@
+import {useEffect} from 'react'
 import socketIO from "socket.io-client";
 
 export default function Messages() {
-function  handleClick() {
-    const socket = socketIO.connect("http://localhost:3001", {
-      withCredentials: true
-     })
-    socket.on("messageResponse", ()=>{console.log("hello")});
+
+  useEffect(()=>{
+    function  connection() {
+      const socket = socketIO.connect("http://localhost:3001", {
+        withCredentials: true
+       })
+      socket.on("messageResponse", ()=>{console.log("hello")});
+    } connection()
+  
+  }, [])
+
+
+  async function handleClick(){
+    const response = await fetch('http://localhost:3000/api/homegrown/public/messages/tPcGjFOvZQQLjHGDOJ1Zea1vho92')
+    const data = await response.json()
+    console.log(data.payload)
+
   }
 
   return <button onClick={handleClick}>Connect</button>;
 }
+  
